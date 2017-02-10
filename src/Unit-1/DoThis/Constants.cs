b8a8e9af2e44008af5201751ff2805c3c7276238
SE_SWSC\kristoffer.rosvall <kristoffer.rosvall@swarco.com>
2017-02-10 12:15:57 +0100

    Lesson 1 complete

---
+++ src/Unit-1/DoThis/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+    public class Messages
+    {
+        public static readonly string CONTINUE = "continue";
+        public static readonly string START = "start";
+    }
+}
